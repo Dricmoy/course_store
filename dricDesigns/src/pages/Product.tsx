98e07ed5c23f5690782f67c40953529646cc7ec4
Dricmoy Bhattacharjee <dricbhattacharjee@gmail.com>
2024-07-30 06:56:30 -0600

    Product page added, needs to be redone Checkout needs to be connected to stripe

--- dricDesigns/src/pages/Product.tsx
+++ dricDesigns/src/pages/Product.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useNavigate } from 'react-router-dom'
 
 type Image = {
     src: string,
@@ -13,8 +14,14 @@ type Props = {
 }
 
 
+
 export default class Product extends React.Component<Props> {
   public render() {
+    const navigate = useNavigate();
+    const handlePayClick = () => {
+        navigate('/checkout');
+    }
+    
     return (
       <div className='container p-0 mt-10 border-l border-r border-b rounded grid grid-cols-3'>
         <img className="col-span-4 w-full h-[80vh] object-cover" src={this.props.images[0].src} alt={this.props.images[0].alt}></img>
@@ -23,7 +30,7 @@ export default class Product extends React.Component<Props> {
             <p className='text-gray-900 p-6'>{this.props.description}</p>
         </div>  
         <div className='border-l border-t p-6 flex flex-col'>
-            <button className="bg-blue-300 border-black text-white hover:bg-blue-200 font-bold rounded shadow-[0.25rem_0.25rem_0px_0px_rgba(0.0.0.1)] focus-outline:none border-gray-900 hover:border-black border-2">Buy now!</button>
+            <button onClick={handlePayClick} className="bg-blue-300 border-black text-white hover:bg-blue-200 font-bold rounded shadow-[0.25rem_0.25rem_0px_0px_rgba(0.0.0.1)] focus-outline:none border-gray-900 hover:border-black border-2">Buy now!</button>
         </div>
       </div>
     );
